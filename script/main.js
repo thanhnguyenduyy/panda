1b901f8a28f5f6de667c9a4232dcbb3e3358cec0
ThanhND <thanhnd@saisystem.vn>
2024-11-04 01:10:01 +0700

    update code

--- script/main.js
+++ script/main.js
@@ -1,21 +1,10 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
-    Swal.fire({
-        title: 'Do you want to play music in the background?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
-    }).then((result) => {
-        if (result.isConfirmed) {
-            document.querySelector('.song').play();
-            animationTimeline();
-        } else {
-            animationTimeline();
-        }
-    });
+    // Automatically play the music
+    document.querySelector('.song').play();
+
+    // Start the animation timeline
+    animationTimeline();
 });
 
 
